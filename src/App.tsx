8e99b5ea56eef68099410db241753f91350f1fa6
AdityaKushwaha <miaditya.kushwaha@gmail.com>
2026-03-28 18:59:04 +0530

    speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
-import { Wordle } from './Wordle/Wordle'
+import "./App.css";
+import { Wordle } from "./Wordle/Wordle";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
-
-  return <Wordle word="LEVEL" attempts={6} />
-  
+	return (
+		<>
+			<Wordle word="LEVEL" attempts={6} /> 
+      <SpeedInsights />
+		</>
+	);
 }
 
-export default App
+export default App;
